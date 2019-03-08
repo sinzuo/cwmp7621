012a3dcdc89bc11ab2dcf896c5ada41be94a532d
root <1586706912@qq.com>
2019-03-08 14:21:24 +0800

    1.xiugai

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cp default_openwrt_s21_config ../openwrt_cat6/config/
-cp tz_form_network ../openwrt_cat6/project/s21/rootfs/etc/tozed/tz_form_network 
-cp firewall.config ../openwrt_cat6/package/network/config/firewall/files/firewall.config
-cp dropbear.config ../openwrt_cat6/package/network/services/dropbear/files/dropbear.config
+PROJECT=openwrt_cat6
+cp default_openwrt_s21_config ../$PROJECT/config/
+cp tz_form_network ../$PROJECT/project/s21/rootfs/etc/tozed/tz_form_network 
+cp firewall.config ../$PROJECT/package/network/config/firewall/files/firewall.config
+cp dropbear.config ../$PROJECT/package/network/services/dropbear/files/dropbear.config
